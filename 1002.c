40c2e42b96935a3190644dc05d2a3ae922323dc2
keac <admin@wgpsec.org>
2017-10-21 20:55:38 +0800

    Add

--- 1002.c
+++ 1002.c
@@ -1,8 +1,2 @@
 #include <stdio.h>
-long long main(void){
-	long long a,b;
-	while(scanf("%lld %lld",&a,&b)!=EOF){
-	printf("%lld + %lld = %lld",a,b,a+b);
-	}
-	return 0;
-}
+
---
+++ 1091.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+	int a,b;
+	while(scanf("%d %d",&a,&b)!=EOF){
+		if(a+b==0) continue;
+		printf("%d\n",a+b);
+	}
+	
+}
