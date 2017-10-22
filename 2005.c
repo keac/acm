ef55bef26585fc960fe9bc2d12fc15db2cbd41b4
keac <admin@wgpsec.org>
2017-10-22 11:49:01 +0800

    fix

--- 2005.c
+++ 2005.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(void){
-	int i,year,mon,day,sum=0;
+	int i,year,mon,day,sum;
 	int t[12]={31,28,31,30,31,30,31,31,30,31,30};
 	while(scanf("%d/%d/%d",&year,&mon,&day)!=EOF){
+		sum=0;
 		for(i=0;i<=mon-2;i++){
 			sum+=t[i];
 		}
-		if((year%400 == 0 || (year %4 == 0 && year %100 != 0))&&mon>2)
+		if((year%400 == 0 || (year %4 == 0 && year %100 != 0))&&mon>=3)
 		sum++;
 		sum+=day;
 		printf("%d\n",sum);
